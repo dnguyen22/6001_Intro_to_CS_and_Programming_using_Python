76811145fc466445d80ff060e1d9c70e320596e7
dnguyen22 <don.nguyen22@gmail.com>
2017-10-26 13:59:12 -0500

    Add comments

--- pset1_3.py
+++ pset1_3.py
@@ -3,14 +3,22 @@
 word = 'zyxwvutsrqp'
 
 
+# Returns longest substring in s where each letter is alphabetical order
 def longest_substring(s):
+    # Length of substring
     longest_count = 1
+    # Starting index of substring
     longest_index = 0
+    # Loop through word
     for letter in range(len(s)-1):
+        # Temporary count of length
         count = 1
+        # Iterate through remainder of string
         for i in range((letter+1), len(s)):
+            # Check if alphabetical
             if s[i-1] <= s[i]:
                 count += 1
+                # Update variables if current count is the longest
                 if count > longest_count:
                     longest_count = count
                     longest_index = letter
