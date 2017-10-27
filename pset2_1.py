041d183b2dfa96f70a232580ebbe7c7f74510f0c
dnguyen22 <don.nguyen22@gmail.com>
2017-10-26 20:45:52 -0500

    Put yearly calculation into function

--- pset2_1.py
+++ pset2_1.py
@@ -1,5 +1,6 @@
 # Paying Debt off in a Year
 
+
 def get_balance(balance, annualInterestRate, monthlyPaymentRate):
     """
     :param balance: the outstanding balance on the credit card
@@ -14,16 +15,33 @@ def get_balance(balance, annualInterestRate, monthlyPaymentRate):
 
     return round(updatedBalance, 2)
 
-init_balance = 42
+balance = 80
 annualInterestRate = 0.2
 monthlyPaymentRate = 0.04
 
-start_balance = init_balance
 
-for month in range(1,13):
-    end_balance = get_balance(start_balance, annualInterestRate, monthlyPaymentRate)
-    print("Month {} Remaining balance: {}".format(month, end_balance))
-    start_balance = end_balance
+def find_min_fixed_monthly_payment():
+    pass
 
-print("Remaining balance: " + str(end_balance))
 
+def get_year_balance(balance, annualInterestRate, monthlyPaymentRate):
+    """
+    Calculates balance at the end of one year (12 months)
+    :param balance: the outstanding balance on the credit card
+    :param annualInterestRate: annual interest rate as a decimal
+    :param monthlyPaymentRate: minimum monthly payment rate as a decimal
+    :return: End of year balance
+    """
+
+    start_balance = balance
+
+    for month in range(1, 13):
+        end_balance = get_balance(start_balance, annualInterestRate, monthlyPaymentRate)
+        #print("Month {} Remaining balance: {}".format(month, end_balance))
+        start_balance = end_balance
+
+    return end_balance
+
+
+end_bal = get_year_balance(balance, annualInterestRate, monthlyPaymentRate)
+print("Remaining balance: " + str(end_bal))
