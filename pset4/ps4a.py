f30dba7a8920735812e6c1b5595f9266f55d3c11
dnguyen22 <don.nguyen22@gmail.com>
2017-10-30 07:39:16 -0500

    Implement getWordScore

--- pset4/ps4a.py
+++ pset4/ps4a.py
@@ -71,7 +71,16 @@ def getWordScore(word, n):
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
-    # TO DO ... <-- Remove this comment when you code this function
+    score = 0
+    for letter in word:
+        score += SCRABBLE_LETTER_VALUES.get(letter, 0)
+
+    score *= len(word)
+
+    if  len(word) == n:
+        score += 50
+    
+    return score
 
 
 
